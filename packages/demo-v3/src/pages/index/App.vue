e30d9ad8caa1238d575c2742930cbec6f498cc95
Liu Jun <870395192@qq.com>
2021-01-04 18:47:16 +0800

    feat: 初始化

---
+++ packages/demo-v3/src/pages/index/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div :class="$style.wrap">
+        <footer :class="$style.footer">
+            <a href="https://github.com/lljj-x/vue-json-schema-form">vue-json-schema-form</a>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+    };
+</script>
+
+<style module>
+    .footer {
+        font-size: 14px;
+        padding: 10px 0;
+        text-align: center;
+    }
+</style>
